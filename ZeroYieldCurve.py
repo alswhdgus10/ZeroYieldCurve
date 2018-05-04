5d260e72b04727aaa8952a7c58969b483b215d63
alswhdgus10 <alswhdgus10@gmail.com>
2018-05-04 10:43:57 +0900

    d'

--- ZeroYieldCurve.py
+++ ZeroYieldCurve.py
@@ -1,32 +1,86 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+import os
 
-rate=pd.read_excel("rate.xlsx",index_col=0)
+os.getcwd() # 기본 디렉토리 확인
+os.chdir("C:/Users/mjh/Documents/ZeroYieldCurve")
 
-def ns(cal1):
-    return sum((rate['YLD_YTM_MID']-(cal1[0]+cal1[1]*(1-np.exp(-m/cal1[3]))/(m/cal1[3])+cal1[2]*((1-np.exp(-m/cal1[3]))/(m/cal1[3])-np.exp(-m/cal1[3]))))**2)
-def sv(cal2):
-    return sum((rate['YLD_YTM_MID']-(cal2[0]+cal2[1]*(1-np.exp(-m/cal2[4]))/(m/cal2[4])+cal2[2]*((1-np.exp(-m/cal2[4]))/(m/cal2[4])-np.exp(-m/cal2[4]))+cal2[3]*((1-np.exp(-m/cal2[5]))/(m/cal2[5])-np.exp(-m/cal2[5]))))**2)
+df = pd.read_excel("fit1.xlsx", sheetname = 0, header=0, index_col=0)
 
-cal1=np.array([0.01,0.01,0.01,0.1])#순서대로 베타0,1,2 타우1
-cal2=np.array([0.01,0.01,0.01,0.01,0.1,0.1]) #순서대로 베타0,1,2,3 타우1,2
+r_nelson = lambda x,m: x[0]+(x[1]*(1-(np.exp(-m/x[3])))*(x[3]/m))+(x[2]*(((1-(np.exp(-m/x[3]))*(x[3]/m))-np.exp(-m/x[3]))))
+d_nelson = lambda x,m: np.exp(-r_nelson(x,m)*m/100)
+r_svensson = lambda x,m: x[0]+(x[1]*(1-(np.exp(-m/x[4])))*(x[4]/m))+(x[2]*(((1-(np.exp(-m/x[4]))*(x[4]/m))-np.exp(-m/x[4]))))+(x[3]*(((1-(np.exp(-m/x[5]))*(x[5]/m))-np.exp(-m/x[5]))))
+d_svensson = lambda x,m: np.exp(-r_svensson(x,m)*m/100)
 
-m=rate['MATURITY_ANNUAL']
+def nelson(x):
+    obj=0
+    for i in range(0,4):#무이표채
+        price = df.iloc[i,0]
+        m = df.iloc[i,5]
+        duration = df.iloc[i,4]
+        price_e = d_nelson(x,m)*100
+        phi = duration*price
+        obj_dis = ((price-price_e)/phi)**2
+        obj = obj+obj_dis
+    
+    for k in range(4,df.index.size): #할인채
+        start = df.iloc[k,6]
+        price = df.iloc[k,0]
+        m = df.iloc[k,5]
+        coupon = (df.iloc[k,1]) /2
+        duration = df.iloc[k,4]
+        phi = duration*price
+        price_ee = 0
+        for j in range(0,1+int(((m-start)*2))):
+            q = start+(0.5*j)
+            price_ee = price_ee +(d_nelson(x,q)*coupon)
+        price_ee = price_ee+(d_nelson(x,m)*100)
+        obj_cou = ((price - price_ee)/phi)**2
+        obj = obj+obj_cou
+    return obj
 
-#Neslon Siegel Model
-res1=minimize(ns,cal1,method='nelder-mead')
-res2=minimize(sv,cal2,method='nelder-mead')
+x0 = [7.69,-4.13,-2.44,2.02]
+res = minimize(nelson,x0,method='Nelder-Mead', tol=1e-10)
+a=res.x
 
-cal1=res1.x
-cal2=res2.x
+def svensson(x):
+    obj=0
+    for i in range(0,4):#무이표채
+        price = df.iloc[i,0]
+        m = df.iloc[i,5]
+        duration = df.iloc[i,4]
+        price_e = d_svensson(x,m)*100
+        phi = duration*price
+        obj_dis = ((price-price_e)/phi)**2
+        obj = obj+obj_dis
+    
+    for k in range(4,df.index.size): #할인채
+        start = df.iloc[k,6]
+        price = df.iloc[k,0]
+        m = df.iloc[k,5]
+        coupon = (df.iloc[k,1]) /2
+        duration = df.iloc[k,4]
+        phi = duration*price
+        price_ee = 0
+        for j in range(0,1+int(((m-start)*2))):
+            q = start+(0.5*j)
+            price_ee = price_ee +(d_svensson(x,q)*coupon)
+        price_ee = price_ee+(100*d_svensson(x,m))
+        obj_cou = ((price - price_ee)/phi)**2
+        obj = obj+obj_cou
+    return obj
 
-ns=(cal1[0]+cal1[1]*(1-np.exp(-m/cal1[3]))/(m/cal1[3])+cal1[2]*((1-np.exp(-m/cal1[3]))/(m/cal1[3])-np.exp(-m/cal1[3])))
-sv=(cal2[0]+cal2[1]*(1-np.exp(-m/cal2[4]))/(m/cal2[4])+cal2[2]*((1-np.exp(-m/cal2[4]))/(m/cal2[4])-np.exp(-m/cal2[4]))+cal2[3]*((1-np.exp(-m/cal2[5]))/(m/cal2[5])-np.exp(-m/cal2[5])))
+x0 = [5.82,-2.55,-0.87,0.45,3.90,0.44]
+res = minimize(svensson,x0,method='Nelder-Mead', tol=1e-10)
+b=res.x
 
-plt.plot(m,ns,'s-')
-plt.show()
-
-plt.plot(m,sv,'s-')
+c = np.linspace(0.1,30,101)
+d= r_nelson(a,c)
+e= r_svensson(b,c)
+plt.plot(c,d/100, label='NS')
+plt.plot(c,e/100, label='SV')
+plt.legend(loc='upper left')
+plt.plot(df['tillMat'],df['YLD_YTM_MID']/100,'gs')
 plt.show()
